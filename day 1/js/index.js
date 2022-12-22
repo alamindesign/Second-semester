9aa4cf1c4e95c4b29811615fec2f48193d0fd0b9
alamindesign <mail.muhammadit@gmail.com>
2022-12-22 12:46:39 +0600

    for the very first time I have created something.

--- day 1/js/index.js
+++ day 1/js/index.js
@@ -1,6 +1,7 @@
 //Function....
-function squre(theNumber){
-    return theNumber*theNumber;
+function squre(){
+    document.getElementById("text").innerHTML="The square of the Number is: "+ document.getElementById("input-number").value*document.getElementById("input-number").value;
 }
-let inputNum= document.getElementById("input-number");
-document.getElementById("text").innerHTML=squre(inputNum);
+let inputNum = document.getElementById("input-number").value;
+let btn= document.getElementById("go-button");
+btn.addEventListener("click",squre);
--- day 1/js/user.js
+++ day 1/js/user.js
@@ -3,5 +3,9 @@ function userPass(user,pass){
     document.write("<br/>Password: "+pass);
 }
 let userName = prompt("Enter User name: ");
-let passWord = prompt("Enter password");
+let passWord = prompt("Enter password: ");
 userPass(userName,passWord);
+//IIFE(Immediately Invokeable Function Expressions)
+(function show(){
+    document.write("<br/>Hello, world");
+})();
