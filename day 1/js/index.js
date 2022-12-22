cd50b9b37b284cd6542af04c9850fc6cd98f7e72
alamindesign <mail.muhammadit@gmail.com>
2022-12-22 11:29:17 +0600

    first commit

---
+++ day 1/js/index.js
@@ -0,0 +1,6 @@
+//Function....
+function squre(theNumber){
+    return theNumber*theNumber;
+}
+let inputNum= document.getElementById("input-number");
+document.getElementById("text").innerHTML=squre(inputNum);
