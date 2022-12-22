e689ad3bc3f17b204bb8a0ef747bce75375d13f4
alamindesign <mail.muhammadit@gmail.com>
2022-12-22 16:31:18 +0600

    getting input for a fixed size array

--- day 1/js/array.js
+++ day 1/js/array.js
@@ -1,4 +1,12 @@
-let hobbies = ['Programming','Gamming','Football','Music','Bedmintor']
-for(i=0;i<hobbies.length;i++){
-    document.write(hobbies[i]+"<br/>");
+// let hobbies = ['Programming','Gamming','Football','Music','Bedmintor']
+// for(i=0;i<hobbies.length;i++){
+//     document.write(hobbies[i]+"<br/>");
+// }
+let names=new Array(5);
+for(i=0;i<names.length;i++){
+    names[i]= prompt("Enter a name: ");
+}
+
+for(i=0;i<names.length;i++){
+    document.write(names[i]+"<br/>");
 }
