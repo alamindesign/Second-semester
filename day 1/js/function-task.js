383da13aecf8397bd9b5955debe634aa32ff9008
alamindesign <mail.muhammadit@gmail.com>
2022-12-22 11:44:08 +0600

    second commit

---
+++ day 1/js/function-task.js
@@ -0,0 +1,15 @@
+function mathMetics(){
+    let N1 = 10;
+    let N2 = 20;
+    let N3 = 30;
+    document.write("<br/>The addition is: "+(N1+N2+N3));
+}
+mathMetics();
+
+function secondMathMetice(num1,num2,num3){
+    document.write("<br/>The addition is: "+(num1+num2+num3));
+}
+let n1 = Number(prompt("Enter first Number: "));
+let n2 = Number(prompt("Enter second Number: "));
+let n3 = Number(prompt("Enter third Number: "));
+secondMathMetice(n1,n2,n3);
